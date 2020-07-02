1c1cd16e414dcd0b96d16e4bcca02c2187bab5a6
Edwin Campuzano <campuzano.edwin1995@gmail.com>
2020-07-01 20:22:11 -0500

    added routes/admin

--- Assignment3-Backend/routes/admin.js
+++ Assignment3-Backend/routes/admin.js
@@ -1,10 +1,46 @@
 const express = require("express");
-const adminController = require("../controllers/admin");
+const adminController = require("../controllers/controlleradmin.txt");
 const router = express.Router();
 
-//if user is authenticated
+// use destructuring because it's an object
+const { check, body } = require("express-validator/check");
 
-// /admin/test => GET
-router.get("/test", adminController.getTesting);
+//add profile to database
+router.post(
+  "/addprofile",
+  [
+    check("fullName")
+      .isLength({
+        max: 50,
+      })
+      .withMessage("Full Name is required"),
+    check("addressOne")
+      .isLength({
+        max: 100,
+      })
+      .withMessage("Address One is required"),
+    check("addressTwo")
+      .isLength({
+        max: 100,
+      })
+      .withMessage("Address Two is optional"),
+    check("city")
+      .isLength({
+        max: 100,
+      })
+      .withMessage("City is required"),
+    check("state").withMessage("State is required"),
+    check("zip")
+      .isLength({
+        min: 5,
+        max: 9,
+      })
+      .withMessage("Zip is required and must be between 5-9 numbers"),
+  ],
+  adminController.postProfile
+);
+
+//get details about profile from database
+router.get("/getprofile", adminController.getProfile);
 
 module.exports = router;
