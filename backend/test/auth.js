cf7439a2cb113caf0fb29a3ddc057ae35218f2a0
salman hoosein <salmanhoosein13@gmail.com>
2020-07-08 13:23:42 -0500

    working testing

--- backend/test/auth.js
+++ backend/test/auth.js
@@ -12,74 +12,79 @@ let newUser = {
 
 //test authentication routes
 describe("Testing Auth Routes", () => {
-  it("It should register a new valid user", (done) => {
-    chai
-      .request(app)
-      .post("/auth/register")
-      .send(newUser)
-      .end(function (err, res) {
-        res.should.have.status(201);
-        res.body.should.have.property("success");
-        done();
-      });
+  describe("Auth Validations", () => {
+    it("it should NOT ADD a user without valid email field", (done) => {
+      let noEmail = JSON.parse(JSON.stringify(newUser));
+      noEmail.email = null;
+      chai
+        .request(app)
+        .post("/auth/register")
+        .send(noEmail)
+        .end((err, res) => {
+          res.body.should.have.property("error");
+          done();
+        });
+    });
+    it("it should NOT ADD a user without valid password field", (done) => {
+      let noPass = JSON.parse(JSON.stringify(newUser));
+      noPass.password = null;
+      chai
+        .request(app)
+        .post("/auth/register")
+        .send(noPass)
+        .end((err, res) => {
+          res.body.should.have.property("error");
+          done();
+        });
+    });
+
+    it("it should NOT LOGIN user without valid email field", (done) => {
+      let noEmail = JSON.parse(JSON.stringify(newUser));
+      noEmail.email = null;
+      chai
+        .request(app)
+        .post("/auth/login")
+        .send(noEmail)
+        .end((err, res) => {
+          res.body.should.have.property("error");
+          done();
+        });
+    });
+    it("it should NOT LOGIN user without valid password field", (done) => {
+      let noPass = JSON.parse(JSON.stringify(newUser));
+      noPass.password = null;
+      chai
+        .request(app)
+        .post("/auth/login")
+        .send(noPass)
+        .end((err, res) => {
+          res.body.should.have.property("error");
+          done();
+        });
+    });
   });
-  it("it should NOT ADD a user without valid email field", (done) => {
-    let noEmail = JSON.parse(JSON.stringify(newUser));
-    noEmail.email = null;
-    chai
-      .request(app)
-      .post("/auth/register")
-      .send(noEmail)
-      .end((err, res) => {
-        res.body.should.have.property("error");
-        done();
-      });
-  });
-  it("it should NOT ADD a user without valid password field", (done) => {
-    let noPass = JSON.parse(JSON.stringify(newUser));
-    noPass.password = null;
-    chai
-      .request(app)
-      .post("/auth/register")
-      .send(noPass)
-      .end((err, res) => {
-        res.body.should.have.property("error");
-        done();
-      });
-  });
-  it("It should login valid user in", (done) => {
-    chai
-      .request(app)
-      .post("/auth/login")
-      .send(newUser)
-      .end(function (err, res) {
-        res.should.have.status(200);
-        res.body.should.have.property("success");
-        done();
-      });
-  });
-  it("it should NOT LOGIN user without valid email field", (done) => {
-    let noEmail = JSON.parse(JSON.stringify(newUser));
-    noEmail.email = null;
-    chai
-      .request(app)
-      .post("/auth/login")
-      .send(noEmail)
-      .end((err, res) => {
-        res.body.should.have.property("error");
-        done();
-      });
-  });
-  it("it should NOT LOGIN user without valid password field", (done) => {
-    let noPass = JSON.parse(JSON.stringify(newUser));
-    noPass.password = null;
-    chai
-      .request(app)
-      .post("/auth/login")
-      .send(noPass)
-      .end((err, res) => {
-        res.body.should.have.property("error");
-        done();
-      });
+  describe("Auth Database/Controllers", () => {
+    it("It should register a new valid user", (done) => {
+      chai
+        .request(app)
+        .post("/auth/register")
+        .send(newUser)
+        .end(function (err, res) {
+          res.should.have.status(201);
+          res.body.should.have.property("success");
+          done();
+        });
+    });
+    it("It should login valid user in", (done) => {
+      chai
+        .request(app)
+        .post("/auth/login")
+        .send(newUser)
+        .end(function (err, res) {
+          res.should.have.status(200);
+          res.body.should.have.property("success");
+          done();
+        });
+    });
   });
 });
