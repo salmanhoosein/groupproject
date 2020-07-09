b4581f10f1ee98af75d076aed23d39e28f76452d
Edwin Campuzano <campuzano.edwin1995@gmail.com>
2020-07-08 20:43:40 -0500

    added routes/auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,43 +5,41 @@ const { check } = require("express-validator/check");
 
 const User = require("../database/user");
 
-
 router.post(
-  "/login",
-  [
-    check("email").isEmail().withMessage("Enter a valid email"),
-    check("password", "Invalid Password").isLength({
-      min: 1,
-    }),
-  ],
-  authController.postLogin
+    "/login",
+    [
+        check("email").isEmail().withMessage("Enter a valid email"),
+        check("password", "Invalid Password").isLength({
+            min: 1,
+        }),
+    ],
+    authController.postLogin
 );
 
 router.post(
-  "/register",
-  [
-    check("email")
-      .isEmail()
-      .withMessage("Enter a valid email")
-      .isLength({
-        min: 1,
-      })
-      .normalizeEmail(), //make all lowercase //Sanitizing
-    // .custom((value, { req }) => {
-    //   return User.findByEmail({
-    //     email: value, //if email is found
-    //   }).then((userDoc) => {
-    //     //if email exists in system, send error
-    //     if (userDoc) {
-    //       return Promise.reject("Email already exists"); //
-    //     }
-    //   });
-    // }),
-    check("password", "Invalid Password").isLength({
-      min: 1,
-    }),
-  ],
-  authController.postRegister
+    "/register",
+    [
+        check("email")
+            .isEmail()
+            .withMessage("Enter a valid email")
+            .isLength({
+                min: 1,
+            })
+            .normalizeEmail()
+            //no duplicate emails
+            .custom((value, { req }) => {
+                return User.findUserByEmail(value).then((user) => {
+                    //if email exists in system, send error
+                    if (user[0].length > 0) {
+                        return Promise.reject("Email already exists");
+                    }
+                });
+            }),
+        check("password", "Invalid Password").isLength({
+            min: 1,
+        }),
+    ],
+    authController.postRegister
 );
 
 module.exports = router;
